2eb12a68e5fac70f4abcacc535c2bc00c5c39828
Claudio Alvarez <cjalvare@gmail.com>
2018-04-13 14:26:32 -0300

    Added a friendly comment to the cfa.r file.

--- cfa.r
+++ cfa.r
@@ -3,5 +3,7 @@ require(ltm)
 eo_error_competence <- data.frame(eodata$E2, eodata$E7, eodata$E31, eodata$E33)
 eo_learning_from_errors <- data.frame(eodata$E21, eodata$E28, eodata$E22, eodata$E3)
 
+# Compute Cronbach's alpha for each construct in the EO questionnaire
 cronbach.alpha(eo_error_competence, standardized = FALSE, CI = FALSE)
 cronbach.alpha(eo_learning_from_errors, standardized = FALSE, CI = FALSE)
+
